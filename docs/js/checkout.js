bd93cc52d89a3de062edeccb2b2aa30ce5618272
Valerii Navalnyi <adultvalerka@gmail.com>
2025-12-10 22:57:06 +0000

    Checkout functionality

--- docs/js/checkout.js
+++ docs/js/checkout.js
@@ -1,50 +1,55 @@
-// checkout.js
+(() => {
+  
+    let checkout = document.getElementById('buy-now');
 
-var checkout = document.getElementById('buy-now');
-// add a listener for add to cart if such a button id is pressed
-var element = document.getElementById("payment-failure");
-element.style.display = 'none';
-var element = document.getElementById("payment-success");
-element.style.display = 'none';
+    let paymentFailure = document.getElementById('payment-failure');
+    paymentFailure.classList.add('d-none');
+    let paymentSuccess = document.getElementById('payment-success');
+    paymentSuccess.classList.add('d-none');
 
-var loggedin=localStorage.getItem('loggedIn'); 
-if (loggedin==0) {
-    window.location.href = "login.html";  // redirect to login page
-}
-// add a listener so that we run this code and preventdefault for submit...
-checkout.addEventListener("click", (event) => {
-    event.preventDefault();
-    var cardnumber=document.getElementById('cardNumber').value;
-    var cardcvv=document.getElementById('cardCvv').value;
-
-    if (cardnumber=="1234 5678 9102 3456" && cardcvv=="123") {
-        alert("payment success");
-        var element = document.getElementById("payment-failure");
-        element.style.display = 'none';
-        //element.classList.add("d-none"); // bootstrap hide
-        var element = document.getElementById("payment-success");
-        element.style.display = 'block';
-        //element.classList.remove("d-none"); // bootstrap hide
-        // now set cart total to zero
-        var total=0;
-        // makes sure that when we goto another page the total is zero 
-        localStorage.setItem('checkout',total); 
-
-    } else {
-        alert("payment failure");
-        var element = document.getElementById("payment-failure");
-        element.style.display = 'block';
-        var element = document.getElementById("payment-success");
-        element.style.display = 'none';
-        //element.classList.add("d-none");
-        var element = document.getElementById("payment-failure");
-        //element.classList.remove("d-none");
- 
+    let loggedin = localStorage.getItem('loggedIn'); 
+    if (loggedin === '0') {
+        window.location.href = 'login.html';  // redirect to login page if not logged in
     }
 
+    checkout.addEventListener('click', completeCheckout)
 
-    
-    
-})
+    function completeCheckout(event) {
+        event.preventDefault();
+        let cardnumber = document.getElementById('cardNumber').value;
+        let cardcvv = document.getElementById('cardCvv').value;
+
+        if (cardnumber === '1234 5678 9102 3456' && cardcvv === '123') {
+            showPSuccess();
+            // now set cart total to zero
+            let total = 0;
+            // makes sure that when we go to another page the total is zero 
+            localStorage.setItem('checkout', total); 
+
+        } else {
+            showPFailure();
+            
+        }
+    }
+
+    // shorthand to show Payment Failure
+    function showPFailure() {
+        paymentFailure.classList.remove('d-none');
+        paymentFailure.classList.add('d-block');
+        paymentSuccess.classList.remove('d-block');
+        paymentSuccess.classList.add('d-none');
+    }
+
+    function showPSuccess() {
+        paymentFailure.classList.remove('d-block');
+        paymentFailure.classList.add('d-none');
+        paymentSuccess.classList.remove('d-none');
+        paymentSuccess.classList.add('d-block');
+    }
+})();
+
+
+
+
 
 
